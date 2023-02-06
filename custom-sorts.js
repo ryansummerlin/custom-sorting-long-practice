9d9ec958f7cfa5d1db67fc6b60eef76bdf71d480
ryansummerlin <ryansummerlin777@gmail.com>
2023-02-05 16:25:57 -0800

    Created all sort functions, passed all tests

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,21 +1,61 @@
 function ageSort(users) {
-  // Your code here
+  const ageCompare = function(a, b) {
+    return a.age - b.age;
+  }
+  return users.sort(ageCompare);
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  const oddEvenCompare = function(a, b) {
+    if (a % 2 === 1 && b % 2 === 0) return -1;
+    if (a % 2 === 0 && b % 2 === 1) return 1;
+    return a - b;
+  }
+
+  return arr.sort(oddEvenCompare);
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  let sSorted = s.split('').sort().join('');
+  let tSorted = t.split('').sort().join('');
+
+  return sSorted === tSorted;
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  strArr = arr.map(num => num.toString());
+
+  const reverseBaseCompare = function(a, b) {
+    if (a.length === b.length) {
+      return a - b;
+    } else {
+      return b.length - a.length;
+    }
+  }
+
+  let strSorted = strArr.sort(reverseBaseCompare);
+  return strSorted.map(str => parseInt(str, 10));
 }
 
 function frequencySort(arr) {
-  // Your code here
+  let frequency = {};
+  arr.forEach(num => {
+    if (frequency[num]) {
+      frequency[num]++;
+    } else {
+      frequency[num] = 1;
+    }
+  });
+
+  const frequencyCompare = function(a, b) {
+    if (frequency[a] === frequency[b]) {
+      return arr.indexOf(b) - arr.indexOf(a);
+    } else {
+    return frequency[a] - frequency[b];
+    }
+  }
+
+  return arr.sort(frequencyCompare);
 }
 
 module.exports = [
